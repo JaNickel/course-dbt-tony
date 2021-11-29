39d39f277fb52412660dbd3cf297d92bd7601b0d
JaNickel <janickel@ucsc.edu>
2021-11-29 04:28:49 +0000

    updates

--- dbt-greenery/models/example/repeat_user_products.sql
+++ dbt-greenery/models/example/repeat_user_products.sql
@@ -6,17 +6,17 @@
 
 with source_data as (
 
-select dbt_tony_nickel.orders_per_user.order_count, 
-      dbt_tony_nickel.stg_products.name, 
+select orders_per_user.order_count, 
+      products.name, 
       count(1) as ctr 
-from dbt_tony_nickel.orders_per_user, 
-     dbt_tony_nickel.stg_products, 
-     dbt_tony_nickel.stg_orders, 
-     dbt_tony_nickel.stg_order_items 
-where dbt_tony_nickel.orders_per_user.user_id=dbt_tony_nickel.stg_orders.user_id 
-  and dbt_tony_nickel.stg_orders.order_id=dbt_tony_nickel.stg_order_items.order_id 
-  and dbt_tony_nickel.stg_order_items.product_id=dbt_tony_nickel.stg_products.product_id 
-group by orders_per_user.order_count, dbt_tony_nickel.stg_products.name
+FROM {{ ref('orders_per_user') }} orders_per_user
+JOIN {{ ref('stg_orders') }} orders
+  ON orders_per_user.user_id = orders.user_id 
+JOIN {{ ref('stg_order_items') }} items
+  ON orders.order_id =items.order_id
+JOIN {{ ref('stg_products') }} products
+  ON items.product_id = products.product_id
+group by orders_per_user.order_count, products.name
 
 )
 select *
