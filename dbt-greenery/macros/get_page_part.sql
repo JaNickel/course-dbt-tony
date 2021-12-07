cfe168d1f20ef13fd7973eec4f7decb3b24db371
JaNickel <janickel@ucsc.edu>
2021-12-07 16:33:17 +0000

    used split_part in stg_event parse

--- dbt-greenery/macros/get_page_part.sql
+++ dbt-greenery/macros/get_page_part.sql
@@ -1,6 +1,4 @@
 {% macro get_page_part(page_url) %}
-
-   reverse(
-      (left(reverse(page_url), position('/' in reverse(page_url))-1))
-   )
+--   reverse((left(reverse(page_url), position('/' in reverse(page_url))-1)))
+   reverse(split_part(reverse(page_url), '-', 1))
 {% endmacro %}
